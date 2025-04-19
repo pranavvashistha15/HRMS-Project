bf2ee467acb8eedd192d69d54e310bf38227f22c
pranavvashistha15 <vashisthapranav86@gmail.com>
2025-04-19 23:21:17 +0530

    leave calender ui

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LeaveManager from './Leaves'
+import AttendanceComponent from './AttendanceComponent'
+
+export default function App() {
+  return (
+    <LeaveManager/>
+    // <AttendanceComponent/>
+  )
+}
